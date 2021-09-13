6300d07818df70af97da474b9705aa8ef8d6ba1f
Angela Loftus <angelaloftus@yahoo.com>
2021-09-12 23:20:08 -0500

    screenshot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
- 
+//display the current time
+
+var currentTime = document.getElementById("#currentDay")
+currentDay.innerhtml(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+//get any saved items from localStorage
+
+//localStorage.getItem("9am")
+//localStorage.getItem("10am");
