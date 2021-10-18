5d8b27b7f388127aa2a3691bac62eac64c6cb0c9
Angela Loftus <angelaloftus@yahoo.com>
2021-10-18 13:19:11 -0500

    notes for future work

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,45 +2,60 @@
 
 var currentTime = document.getElementById("currentDay")
 var todayDate = (moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+//QUESTION: How do i get this to count seconds? is it easy? instead of static time
 currentTime.innerHTML = "Today's date is " + todayDate;
-// moment().format('MMMM Do YYYY, h:mm:ss a');
+
+//console log to check if moment is working 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-//get any saved items from localStorage
+//area of code where we set and retrieve items from local storage
 
-document.addEventListener("svBtn");
+ // document.addEventListener("svBtn");
 
-svBtn.onClick.localStorage.setItem("hourblock");
+$(".svBtn").on('click', function()
+    {
+    localStorage.setItem("hourblock")
+    });
+    
+    //get any saved items from localStorage
 
-localStorage.getItem("9");
-localStorage.getItem("10");
-localStorage.getItem("11");
-localStorage.getItem("12");
-localStorage.getItem("13");
-localStorage.getItem("14");
-localStorage.getItem("15");
-localStorage.getItem("16");
-localStorage.getItem("17");
+    // localStorage.getItem("9");
+    // localStorage.getItem("10");
+    // localStorage.getItem("11");
+    // localStorage.getItem("12");
+    // localStorage.getItem("13");
+    // localStorage.getItem("14");
+    // localStorage.getItem("15");
+    // localStorage.getItem("16");
+    // localStorage.getItem("17");
 
-var hourblock = document.querySelector("hourblock").value;
 
 function checkTime() {
+    
+var hourblock = document.querySelector("hourblock") ;
 
-if (hourblock < currentTime) {
-    hourblock.addClass("past");
-    hourblock.removeClass("present");
-    hourblock.removeClass("future");
+let currentHour = moment().hour();
+console.log(currentHour);
+
+//this statement returns null--how do I link the hourblocks in the HTML to here?
+console.log(hourblock);
+
+if (hourblock < currentHour) {
+    $("hourblock").addClass("past");
+    $("hourblock").removeClass("present");
+    $("hourblock").removeClass("future");
 }
-else if (hourblock === currentTime) {
-    hourblock.removeClass("past");
-    hourblock.addClass("present");
-    hourblock.removeClass("future");
+else if (hourblock === currentHour) {
+    $("hourblock").removeClass("past");
+    $("hourblock").addClass("present");
+    $("hourblock").removeClass("future");
 }
 
-if (hourblock > currentTime) {
-    hourblock.removeClass("past");
-    hourblock.removeClass("present");
-    hourblock.addClass("future");
+else if (hourblock > currentHour) {
+    $("hourblock").removeClass("past");
+    $("hourblock").removeClass("present");
+    $("hourblock").addClass("future");
 
 }
 }
