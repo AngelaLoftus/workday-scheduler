964d1fe59acbc1a74014af3644060d7e1a986136
Angela Loftus <angelaloftus@yahoo.com>
2021-10-26 12:35:26 -0500

    fixed pm bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,30 +8,32 @@ $(document).ready(function() {
     //console log to check if moment is working 
     console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-
+//function to compare the current hour to the hour on the calendar 
+//and color code accordingly
 function colorCoder () {
-    
+    //current time
     let currentHour = moment().hour();
     
-    
+    //for each hourblock, turn the id string to integer
     $(".hourblock").each(function () {
         let activityHour = parseInt($(this).attr("id"));
         console.log("blockHour", activityHour);
 
-        
+    //if hour is in the past, color it grey
     if (activityHour < currentHour) {
         $(this).addClass("past");
         $(this).removeClass("present");
         $(this).removeClass("future");
         console.log("should show past color");
     }
+    //if hour is in the present, color it red
     else if (activityHour === currentHour) {
         $(this).removeClass("past");
         $(this).addClass("present");
         $(this).removeClass("future");
         console.log("present color here");
     }
-
+    //if hour is in the future, color it green
     else if (activityHour > currentHour) {
         $(this).removeClass("past");
         $(this).removeClass("present");
@@ -41,33 +43,44 @@ function colorCoder () {
 });
 }
 
+//call the color coder function
 colorCoder(); 
 
  
 $(".saveBtn").on('click', function()
 {
     let activityDescription = $(this).siblings(".description").val();
-    let activityTime = $(this).parent().attr("id");
+    let activityTime = parseInt($(this).parent().attr("id"));
     console.log(activityDescription);
     console.log(activityTime);
+    
+    if (activityTime < 12) {
     localStorage.setItem(activityTime + "am", activityDescription);
+    }
+    else if (activityTime >= 12){
+        localStorage.setItem(activityTime + "pm", activityDescription);
+    }  
 });
 
 //load saved data
 let temp = localStorage.getItem("9am");
-console.log("temp 9 am",temp);
+console.log("temp 9 am test",temp);
 
+let temp2 = localStorage.getItem("13pm")
+console.log("'temp 13 pm test'", temp2);
+
+//get all the items from local storage and display to text area on page 
  $('#9 .description').val(localStorage.getItem("9am"));
  $('#10 .description').val(localStorage.getItem("10am"));
  $('#11 .description').val(localStorage.getItem("11am"));
  $('#12 .description').val(localStorage.getItem("12pm"));
- $('#13 .description').val(localStorage.getItem("1pm"));
- $('#14 .description').val(localStorage.getItem("2pm"));
- $('#15 .description').val(localStorage.getItem("3pm"));
- $('#16 .description').val(localStorage.getItem("4pm"));
- $('#17 .description').val(localStorage.getItem("5pm"));
+ $('#13 .description').val(localStorage.getItem("13pm"));
+ $('#14 .description').val(localStorage.getItem("14pm"));
+ $('#15 .description').val(localStorage.getItem("15pm"));
+ $('#16 .description').val(localStorage.getItem("16pm"));
+ $('#17 .description').val(localStorage.getItem("17pm"));
 
- //set up periodic timer to color code every hour 
+ //periodic timer to check the color coding and reiterate every minute 
     let timer = setInterval(colorCoder, 60000);
  
 });
